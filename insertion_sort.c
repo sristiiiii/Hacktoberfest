09acc3be39d1925e46926d613ca4934e33cbacd3
Pawan kumar sah <53905882+pawan8893@users.noreply.github.com>
2021-10-01 00:06:03 +0530

    Create insertion_sort.c

---
+++ insertion_sort.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+void printArray(int arr[], int n)
+{
+    printf("The elements of array are:\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+
+void insertion_sort(int arr[],int n)
+{
+    int key,j,i;
+    for(i=1;i<n;i++)
+    {
+        key=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+
+int main()
+{
+    //*************DRY RUN********************
+    // -1   0    1   2   3   4   5
+   //      12,| 54, 65, 07, 23, 09 --> i=1, key=54, j=0
+   //      12,| 54, 65, 07, 23, 09 --> 1st pass done (i=1)!
+
+   //      12, 54,| 65, 07, 23, 09 --> i=2, key=65, j=1
+   //      12, 54,| 65, 07, 23, 09 --> 2nd pass done (i=2)!
+
+   //      12, 54, 65,| 07, 23, 09 --> i=3, key=7, j=2
+   //      12, 54, 65,| 65, 23, 09 --> i=3, key=7, j=1
+   //      12, 54, 54,| 65, 23, 09 --> i=3, key=7, j=0
+   //      12, 12, 54,| 65, 23, 09 --> i=3, key=7, j=-1
+   //      07, 12, 54,| 65, 23, 09 --> i=3, key=7, j=-1--> 3rd pass done (i=3)!
+
+   // Fast forwarding and 4th and 5th pass will give:
+   //      07, 12, 54, 65,| 23, 09 --> i=4, key=23, j=3
+   //      07, 12, 23, 54,| 65, 09 --> After the 4th pass
+
+   //      07, 12, 23, 54, 65,| 09 --> i=5, key=09, j=4
+   //      07, 09, 12, 23, 54, 65| --> After the 5th pass 
+
+
+    int n;
+    printf("Enter the size of array:\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of array:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printArray(arr,n);
+    insertion_sort(arr,n);
+    printArray(arr,n);
+    return 0;
+}
